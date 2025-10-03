a40f62262ae5a1cb09c1bb50e175a09ffc1e8c2b
attiq-ur-rehman4855 <attiqurrehman812004@gmail.com>
2025-10-03 19:20:49 +0500

    Update next.config.mjs for API rewrite

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,17 +1,4 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {};
-
-// export default nextConfig;
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "https://shop-sphere.infinityfreeapp.com/api/:path*",
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
